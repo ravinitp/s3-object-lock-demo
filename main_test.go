78398616cd082f172a3a2ddaff9875aa33c13424
ravbhart <ravi.prakash.bharti@oracle.com>
2023-10-06 15:48:55 +0530

    unit test improvement

--- main_test.go
+++ main_test.go
@@ -2,32 +2,53 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"testing"
-	"time"
 )
 
 func Test_lock(t *testing.T) {
-	bucket := "my-bucket"
+	bucket := "lock-demo"
 	path := "terraform.tfstate"
+	var LockVersionId = ""
+	mutex := sync.Mutex{}
+	var wg sync.WaitGroup
 	var count = 0
-	for i := 0; i < 10; i++ {
+
+	lockRecieved := make(chan bool)
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			fmt.Printf("Agent joined the race to aquire the lock\n")
 			versionId, err := lock(bucket, path)
 			if err != nil {
 				fmt.Printf("Agent couldn't aquire the lock, versionId - %s -- %q\n", versionId, err)
+				lockRecieved <- false
 			} else {
 				mutex.Lock()
+				LockVersionId = versionId
 				count++
+				lockRecieved <- true
 				mutex.Unlock()
 			}
 		}()
 	}
-	select {
-	case <-time.After(time.Second * 20):
-		if count != 1 {
-			fmt.Printf("count should always 1  : %d\n", count)
-			t.Fail()
+
+	go func() {
+		wg.Wait()
+		close(lockRecieved)
+		unlock(LockVersionId, bucket, path)
+	}()
+
+	var total int64
+	for lr := range lockRecieved {
+		if lr {
+			total++
 		}
+	}
+	if total != 1 && count != 1 {
+		fmt.Printf("total: %d, count: %d\n", total, count)
+		t.Fail()
 	}
 }
